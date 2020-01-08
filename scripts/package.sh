93512b39f56e29fc5fd5d7023ecca264b2d48e5d
Keith <keithwhisper@gmail.com>
2020-01-08 17:25:36 +0800

    chore: add package script

---
+++ scripts/package.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+rm -rf release
+mkdir -p release/popup/public
+cp manifest.json release/manifest.json
+cp background.js release/background.js
+cp content.js release/content.js
+cp content.css release/content.css
+cp options.html release/options.html
+cp options.js release/options.js
+cp -r images release/images
+cp -r popup/public release/popup
+cd release/popup/public/build && rm *.map
